93f4243ccc56aa60b0ebe62f8fb9da7e8daad2bc
PrincyChauhan <66620521+PrincyChauhan@users.noreply.github.com>
2021-04-12 09:53:18 +0530

    DONE

---
+++ Practical 5/src/S1.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S1
+ */
+public class S1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public S1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		
+		ServletConfig config = getServletConfig();
+		
+		out.print("a is" + " "+ config.getInitParameter("a")+ "<br></br>");
+		out.print("b is" + " "+ config.getInitParameter("b")+ "<br></br>");
+		
+		
+		ServletContext context = getServletContext();
+		out.print("x is" + " "+ context.getInitParameter("x"));
+	}
+
+}
---
+++ Practical 5/src/S2.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S2
+ */
+public class S2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public S2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		
+		ServletConfig config = getServletConfig();
+		out.print("c is" + " "+ config.getInitParameter("c")+ "<br></br>");
+        out.print("d is" + " "+ config.getInitParameter("d") + "<br></br>");
+		
+		
+		
+		ServletContext context = getServletContext();
+		out.print("x is" + " "+ context.getInitParameter("x"));
+	}
+}
