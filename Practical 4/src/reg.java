ac8e36587057adb2993790abf04e2360966cd962
PrincyChauhan <66620521+PrincyChauhan@users.noreply.github.com>
2021-04-12 09:55:44 +0530

    DONE

---
+++ Practical 4/src/reg.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class reg
+ */
+@WebServlet("/reg")
+public class reg extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public reg() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String a = request.getParameter("UserId");
+		String b = request.getParameter("email");
+		
+		out.print("<h1>Username is"+" "+a + "<h1></br>");
+		out.print("<h1>Email is"+" "+b+"<h1></br>");
+
+	}
+
+}
